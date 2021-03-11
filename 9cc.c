6798df9ddaa7b44430d6f06778ede4f92cb0aaf8
tanzankai <chintake093612@gmail.com>
2021-03-11 11:16:21 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,33 @@ int main(int argc, char **argv){
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+
+    // 数字以外の文字を見つけるまで数字を出力する
+    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+
+    // pが終端文字に達するまで行う
+    while(*p){
+        if(*p == '+'){
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if(*p == '-'){
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return EXIT_FAILURE;
+    }
+
     printf("    ret\n");
     return 0;
 }
