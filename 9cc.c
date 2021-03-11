8808b9b3a51d8c4c25b33c6d2e97fdcfb8fd2f93
tanzankai <chintake093612@gmail.com>
2021-03-11 13:02:56 +0900

    エラー箇所を指摘する機能を追加

--- 9cc.c
+++ 9cc.c
@@ -1,37 +1,154 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
+#include<stdarg.h>
+#include<stdbool.h>
+#include<string.h>
+
+// トークンの種類
+typedef enum{
+    TK_RESERVED, // 記号
+    TK_NUM, // 整数トークン
+    TK_EOF, // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token{
+    TokenKind kind; // トークンの型(種類)
+    Token *next; // 次の入力トークン
+    int val; // kindがTK_NUMの場合、その数値
+    char *str; // トークン文字列
+};
+
+// 現在着目しているトークン
+Token *token;
+
+// 入力プログラム
+char *user_input;
+
+
+// エラーを報告するための関数
+// printfと同じ引数をとる
+void error(char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
+}
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input; // エラーが発生した位置
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
+}
+
+// 次のトークンが"期待している" "記号" の時にはトークンを1つ読み進める
+// 正常に処理を行うことができれば真を返す そうでなければ偽を返す
+bool consume(char op){
+    if(token->kind != TK_RESERVED || token->str[0] != op) return false;
+    token = token->next;
+    return true;
+}
+
+// 次のトークンが"期待している" "記号" の時にはトークンを1つ読み進める
+// それ以外の場合にはエラーを報告する
+void expect(char op){
+    if(token->kind != TK_RESERVED || token->str[0] != op){
+        error_at(token->str, "'%c'ではありません", op); // ここもエラー時にカーソルで表示できるようにしたい
+    }
+    token = token->next;
+}
+
+// 次のトークンが"数値"ならばトークンを1つ読み進めてその数値を返す
+// それ以外の場合にはエラーを報告する
+int expect_number(){
+    if(token->kind != TK_NUM) error_at(token->str, "数ではありません");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof(){
+    return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurにつなげる
+Token *new_token(TokenKind kind, Token *cur, char *str){
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p){
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while(*p){
+        if(isspace(*p)){
+            p++;
+            continue;
+        }
+
+        if(*p == '+' || *p == '-'){
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+        if(isdigit(*p)){
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error_at(p, "トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p);
+
+    // 入力文字列をトークナイズした結果生成されたトークン列の先頭を返す
+    return head.next;
+}
 
 int main(int argc, char **argv){
     if(argc != 2){
-        fprintf(stderr, "引数の個数が正しくありません\n");
-        return 1;
+        error("引数の個数が正しくありません");
+        return EXIT_FAILURE;
     }
 
-    char *p = argv[1];
+    user_input = argv[1];
+    token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    // 数字以外の文字を見つけるまで数字を出力する
-    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+    // 正しい入力ならば初めは数字であることから
+    // 最初にmov命令を出力
+    printf("    mov rax, %d\n", expect_number());
 
-    // pが終端文字に達するまで行う
-    while(*p){
-        if(*p == '+'){
-            p++;
-            printf("    add rax, %ld\n", strtol(p, &p, 10));
+    // トークン列を消費しつつアセンブリを出力
+    while(!at_eof()){
+        if(consume('+')){
+            printf("    add rax, %d\n", expect_number());
             continue;
         }
 
-        if(*p == '-'){
-            p++;
-            printf("    sub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-
-        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-        return EXIT_FAILURE;
+        expect('-');
+        printf("    sub rax, %d\n", expect_number());
     }
 
     printf("    ret\n");
